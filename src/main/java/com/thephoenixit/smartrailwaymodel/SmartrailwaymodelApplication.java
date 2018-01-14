dee3ffca949485d6b0352bcccad67c668a4e752f
hamzajg <hamzajg@gmail.com>
2018-01-15 08:00:15 +0900

    Init Project...

---
+++ src/main/java/com/thephoenixit/smartrailwaymodel/SmartrailwaymodelApplication.java
@@ -0,0 +1,12 @@
+package com.thephoenixit.smartrailwaymodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartrailwaymodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartrailwaymodelApplication.class, args);
+	}
+}
